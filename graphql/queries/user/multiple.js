b783e7878bbe7628c3af47a8312e43ad12f20aa0
anish.m.prasad <anish.m.prasad@gmail.com>
2018-10-05 00:26:39 +0530

    user update

--- graphql/queries/user/multiple.js
+++ graphql/queries/user/multiple.js
@@ -1,5 +1,16 @@
 import {
   GraphQLList
-} from 'graphql'
-import { userType } from '../../types'
-import UserModal from '../../modals/user'
+} from 'graphql';
+import { userType } from '../../types';
+import UserModal from '../../modals/user';
+
+export default {
+  type : new GraphQLList(userType),
+  resolve(){
+    const users = UserModal.find().exec();
+    if(!users){
+      throw new Error("Error while fetching user...")
+    }
+    return users
+  }
+}
